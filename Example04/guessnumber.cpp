1a5e572218ecfee455b912788f0daf65da9ef9b9
rl1987 <rl1987@sdf.lonestar.org>
2018-11-10 16:42:52 +0200

    Example 04 - number guessing game

---
+++ Example04/guessnumber.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+  int r;
+  int guess = 0;
+
+  srand(time(NULL));
+  r = rand() % 100;
+
+  do {
+    cout << "What's your guess?" << endl;
+
+    if (!(cin >> guess)) {
+      cin.clear();
+      cin.ignore();
+      continue;
+    }
+
+    if (guess < r)
+      cout << "Too low!" << endl;
+
+    if (guess > r)
+      cout << "Too high!" << endl;
+
+  } while (guess != r);
+  
+  cout << "You gueessed it!" << endl;
+
+  return 0;
+}
+
